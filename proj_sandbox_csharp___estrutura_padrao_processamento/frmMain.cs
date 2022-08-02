97aa7005448dff89c162ac3a6b0f27db44d8a677
Eduardo Cordeiro Maia Monteiro <edufacfito@gmail.com>
2022-08-02 19:04:08 -0300

    first commit

---
+++ proj_sandbox_csharp___estrutura_padrao_processamento/frmMain.cs
@@ -0,0 +1,10 @@
+namespace proj_sandbox_csharp___estrutura_padrao_processamento
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
